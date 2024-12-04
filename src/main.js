3cd9826cae4ee2e93acea0dfb03e1beb4ec9e93a
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2024-12-03 22:03:19 -0300

    bootstrap override

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
+import "@/assets/scss/main.scss"
 createApp(App).use(store).use(router).mount("#app");
