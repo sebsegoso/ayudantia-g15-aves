4c46a4844e7f626dcd6711134b4ba9aa805e8831
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2024-12-03 21:02:27 -0300

    creacion de ave card para listar aves en vista avesview

---
+++ src/components/AveCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card h-100">
+        <img :src="ave.images.thumb" :alt="ave.name.spanish" class="card-image-top h-100">
+        <div class="card-body">
+            <h5 class="card-title">{{ ave.name.spanish }}</h5>
+            <p class="card-text"><b>Nombre científico:</b> {{ ave.name.latin }}</p>
+            <router-link class="btn btn-primary" :to="{
+                name: 'ave-detalle',
+                params: {
+                    id: ave.uid
+                }
+            }">Ver detalles</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        ave: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped></style>
--- src/views/AvesView.vue
+++ src/views/AvesView.vue
@@ -1,24 +1,22 @@
 <template>
-    <div>
+    <div class="container">
         <h1>Todas las aves</h1>
-        
-        <ul>
-            <li v-for="ave in aves" :key="ave.uid">
-                <router-link :to="{
-                    name: 'ave-detalle',
-                    params: {
-                        id: ave.uid
-                    }
-                }">{{ ave.name.spanish }}</router-link>
-            </li>
-        </ul>
+
+        <div class="row">
+
+            <div v-for="ave in aves" :key="ave.uid" class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
+                <AveCard :ave="ave" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import AveCard from '@/components/AveCard.vue';
 import { mapState } from 'vuex';
 
 export default {
+    components: { AveCard },
     computed: {
         ...mapState(["aves"])
     }
